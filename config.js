ffaa10b9a1eee678e0724532e1619542a7591b53
Chad Sahlhoff <csahlhoff@gmail.com>
2013-01-09 18:31:47 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
    'secrets' : {
-    'clientId' : 'r6UpA6cvR7ohHZte6PfxHw',
-    'clientSecret' : 'qByZE8iyNpLzZxfp28MrG5lLNyu2JfC6gD3giOrCbI',
+    'clientId' : 'YOURCLIENTID',
+    'clientSecret' : 'YOURCLIENTSECRET',
     'redirectUrl' : 'http://127.0.0.1:3000/auth/twitter/callback'
   }
 };
